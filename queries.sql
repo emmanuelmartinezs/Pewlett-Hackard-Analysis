6fc01bd6831957412a99a7b4db860d28a149fa37
Emmanuel <71406941+emmanuelmartinezs@users.noreply.github.com>
2020-11-08 19:56:47 -0500

    update

--- queries.sql
+++ queries.sql
@@ -115,3 +115,28 @@ FROM retirement_info as ri
 LEFT JOIN dept_emp as de
 ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
+
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+
+-- Export Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retire_by_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
